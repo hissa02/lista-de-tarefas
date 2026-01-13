1659b0c84cbd95618e3642b9260fb417c5f0a186
Hissa Barbara <hissa.barbara@discente.ufma.br>
2026-01-12 23:34:49 -0300

    trocando padrão

--- classe/tarefa.py
+++ classe/tarefa.py
@@ -1,28 +1,8 @@
-from abc import ABC, abstractmethod # para o padrão strategy
-#classes de status implementando o padrão strategy
-class StrategyStatus(ABC):
-    @abstractmethod
-    def definir_status(self):
-        pass
-
-class Disponivel(StrategyStatus):
-    def definir_status(self):
-        return "Disponível"
-
-class Fazendo(StrategyStatus):
-    def definir_status(self):
-        return "Fazendo"
-
-class Feito(StrategyStatus):
-    def definir_status(self):
-        return "Feito"
-    
-#classe tarefa
 class Tarefa:
-    def __init__(self, nome, descricao, strategy_status_definir_status):
+    def __init__(self, nome, descricao, status):
         self.nome = nome
         self.descricao = descricao
-        self.status = strategy_status_definir_status.definir_status()
+        self.status = status
 
     def mudar_status(self, novo_status):
         self.status = novo_status
